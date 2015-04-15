0dc33b96afb78961398eb0533bf759438490c475
Marius Küng <maku@mailbox.org>
2015-04-15 13:31:28 +0200

    Cleanup server

--- SocketChat/src/ch/fhnw/kvan/chat/socket/server/Server.java
+++ SocketChat/src/ch/fhnw/kvan/chat/socket/server/Server.java
@@ -3,12 +3,9 @@ package ch.fhnw.kvan.chat.socket.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 
-import ch.fhnw.kvan.chat.interfaces.IChatDriver;
-import ch.fhnw.kvan.chat.interfaces.IChatRoom;
-import ch.fhnw.kvan.chat.socket.server.ConnectionListener;
 import org.apache.log4j.Logger;
 
-public class Server implements IChatRoom, IChatDriver{
+public class Server {
 
 	private Logger logger;
 	private ServerSocket serverSocket;
@@ -17,8 +14,8 @@ public class Server implements IChatRoom, IChatDriver{
 		logger = Logger.getLogger(Server.class);
 	}
 
-	@Override
-	public void connect(String host, int port) throws IOException {
+	public static void main (String[] args) {
+
 		try{
 			serverSocket = new ServerSocket(port);
 			ConnectionListener connectionListener = new ConnectionListener(serverSocket);
@@ -29,63 +26,7 @@ public class Server implements IChatRoom, IChatDriver{
 
 		} catch (IOException e) {
 			System.out.println("Could not listen on port " + port);
-			System.exit(-1);
 		}
-	}
 
-	@Override
-	public void disconnect() throws IOException {
-		if (serverSocket != null) {
-			serverSocket.close();
-		}
 	}
-
-	@Override
-	public IChatRoom getChatRoom() {
-		// TODO Auto-generated method stub
-		return this;
-	}
-
-	@Override
-	public boolean addParticipant(String name) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean removeParticipant(String name) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addTopic(String topic) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean removeTopic(String topic) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addMessage(String topic, String message) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String getMessages(String topic) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String refresh(String topic) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
