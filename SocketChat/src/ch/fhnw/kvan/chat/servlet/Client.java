32d705d8d267bb891b59e6700b48fa820129cffe
Marius Küng <maku@mailbox.org>
2015-05-09 12:12:02 +0200

    Added client and server setup Fails due to unkown exception

--- SocketChat/src/ch/fhnw/kvan/chat/servlet/Client.java
+++ SocketChat/src/ch/fhnw/kvan/chat/servlet/Client.java
@@ -1,8 +1,123 @@
 package ch.fhnw.kvan.chat.servlet;
 
-public class Client {
+import ch.fhnw.kvan.chat.gui.ClientGUI;
+import ch.fhnw.kvan.chat.interfaces.IChatRoom;
+import ch.fhnw.kvan.chat.utils.In;
+import ch.fhnw.kvan.chat.utils.Out;
 
-	public static void main(String[] args) {
+import java.net.*;
 
+import org.apache.http.HttpRequest;
+import org.apache.log4j.Logger;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.io.IOException;
+
+public class Client implements IChatRoom {
+	private String name;
+	private String path;
+	private ClientGUI gui;
+
+	private Out outputStream;
+	private In inputStream;
+
+    private URI uri;
+    private HttpClient httpClient;
+
+    private static Logger logger = Logger.getLogger(Client.class);
+
+	public static void main(String[] args) throws Exception {
+
+        if (args.length < 2) {
+            throw new Exception("Missing arguments either <url> or <name>");
+        } else {
+            new Client(args[0], args[1]);
+        }
 	}
+
+    public Client (String n, String p) throws IOException, URISyntaxException {
+        this.name = n;
+        this.path = p;
+        this.gui = new ClientGUI(this, this.name);
+
+        this.uri = new URIBuilder().setScheme("http").setHost(this.path).build();
+        this.httpClient = HttpClientBuilder.create().build();
+
+//        HttpUriRequest req = new HttpGet(path + "?action=addParticipant&name=alice");
+//        try {
+//            httpClient.execute(req);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        String url = "http://" + this.path + "?action=addParticipant&name=" + this.name;
+//        // this.outputStream = new Out(url);
+//        // this.outputStream.print();
+//
+//        URL obj = null;
+//        try {
+//            obj = new URL(url);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+//            inputStream = new In(obj);
+//
+//            String inputLine;
+//            StringBuffer response = new StringBuffer();
+//
+//            while ((inputLine = inputStream.readLine()) != null) {
+//                response.append(inputLine);
+//            }
+//            inputStream.close();
+//
+//            //print result
+//            System.out.println(response.toString());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        logger.info("Client instance started");
+    }
+
+    @Override
+    public boolean addParticipant(String name) throws IOException {
+        return false;
+    }
+
+    @Override
+    public boolean removeParticipant(String name) throws IOException {
+        return false;
+    }
+
+    @Override
+    public boolean addTopic(String topic) throws IOException {
+        return false;
+    }
+
+    @Override
+    public boolean removeTopic(String topic) throws IOException {
+        return false;
+    }
+
+    @Override
+    public boolean addMessage(String topic, String message) throws IOException {
+        return false;
+    }
+
+    @Override
+    public String getMessages(String topic) throws IOException {
+        return null;
+    }
+
+    @Override
+    public String refresh(String topic) throws IOException {
+        return null;
+    }
 }
--- SocketChat/src/ch/fhnw/kvan/chat/servlet/Server.java
+++ SocketChat/src/ch/fhnw/kvan/chat/servlet/Server.java
@@ -1,10 +1,25 @@
 package ch.fhnw.kvan.chat.servlet;
-
+import ch.fhnw.kvan.chat.general.ChatRoom;
 import javax.servlet.* ;
 import javax.servlet.http.* ;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
+import org.apache.log4j.Logger;
+
+@WebServlet("/chat/server/participant")
 public class Server extends HttpServlet {
-    public static void main(String[] args) {
+    private static ChatRoom chatRoom = ChatRoom.getInstance();
+    private static Logger logger = Logger.getLogger(Server.class);
 
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        PrintWriter out = response.getWriter();
+        out.println("Hello");
+        logger.info(request);
     }
 }
